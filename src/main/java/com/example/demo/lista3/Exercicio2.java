7e8f7b5d3f9130cfd64f1d023adb503fcec9b250
isabeladp <isabeladp98@gmail.com>
2024-09-04 15:21:05 -0300

    Exercicio2

---
+++ src/main/java/com/example/demo/lista3/Exercicio2.java
@@ -0,0 +1,21 @@
+package com.example.demo.lista3;
+import java.util.Scanner;
+public class Exercicio2 {
+    public static void resolucao3()throws InterruptedException{
+
+    int num1, resultado;
+
+    System.out.print("Digite um numero para visualizar a tabuada dele: \n");
+    Scanner leia = new Scanner(System.in);
+    num1 = leia.nextInt();
+
+    for (int tabuada = 0; tabuada <= 10; tabuada ++) {
+        resultado = num1 * tabuada;
+        System.out.println(num1 + " x " + tabuada + " = " + resultado);
+    }
+}
+}
+
+
+
+
