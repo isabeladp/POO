1eff72cc22588a54ee62642e950fbda2c6ebb208
isabeladp <isabeladp98@gmail.com>
2024-09-04 15:09:45 -0300

    Exercicio1

---
+++ src/main/java/com/example/demo/lista3/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo.lista3;
+
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+    
+    
+    
+    
+    @SpringBootApplication
+    public class DemoApplication {
+    
+        public static void main(String[] args)throws InterruptedException {
+            SpringApplication.run(DemoApplication.class, args);
+            Exercicio1.resolucao3();
+        }
+    
+    }
+
---
+++ src/main/java/com/example/demo/lista3/Exercicio1.java
@@ -0,0 +1,24 @@
+package com.example.demo.lista3;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+    public class Exercicio1{
+        public static void resolucao3()throws InterruptedException{
+
+            int numeroDigitado;
+
+            System.out.println("Digite um número em segundos ente 1 e 59:");
+            Scanner leia = new Scanner(System.in);
+            numeroDigitado = leia.nextInt();
+
+
+    while (numeroDigitado < 1 || numeroDigitado > 59) {
+        System.out.println("Digite um número valido:");
+                numeroDigitado = leia.nextInt();
+    }
+        for (int i = 0; i <= numeroDigitado; i++) {
+                System.out.println( "00:" + i );
+                TimeUnit.SECONDS.sleep(1);
+        }
+    }
+}
