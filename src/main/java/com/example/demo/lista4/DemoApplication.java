d4b0223728c04215c64df693e64cb66ac236f5bf
isabeladp <isabeladp98@gmail.com>
2024-09-05 16:33:58 -0300

    Lista4

---
+++ src/main/java/com/example/demo/lista4/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo.lista4;
+
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+    
+    
+    
+    
+    @SpringBootApplication
+    public class DemoApplication {
+    
+        public static void main(String[] args)throws InterruptedException {
+            SpringApplication.run(DemoApplication.class, args);
+            Exercicio2.resolucao4();
+        }
+    
+    }
+
